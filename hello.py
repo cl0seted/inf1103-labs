fa250f2ca061219b83f83da198f93c215b32d442
Jody <2603025@sit.singaporetech.edu.sg>
2026-09-04 15:03:19 +0800

    Updated, ACT 2

--- hello.py
+++ hello.py
@@ -4,3 +4,11 @@ print("Welcome here")
 print("My first post!")
 print("====================")
 
+# ACTIVITY 2
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
